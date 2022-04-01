5d4a3db212db9ba01aaf84bf02a895e34081f068
kerong <70834651+kerong2002@users.noreply.github.com>
2022-04-01 18:35:17 +0800

    Add files via upload

---
+++ src/a001.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class a001 {
+    public static void main(String[]args){
+        Scanner cin=new Scanner(System.in);
+        String s;
+        while(cin.hasNext()){
+            s=cin.nextLine();
+            System.out.println("hello, "+s);
+        }
+    }
+}
